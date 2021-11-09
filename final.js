a324fd66422cde4d32b5ca1e677d23ee5914e1d9
ChangmingTan <tcm.truman@gmail.com>
2021-11-09 03:49:50 -0800

    Add files via upload

---
+++ final.js
@@ -0,0 +1,15 @@
+"use strict";
+/*
+    Author: Changming Tan
+    Date:   11/30/2018
+    File name: final.js
+
+*/
+
+/*
+
+    This script shows the date of the website created, which is 12/1/2018.
+	replace the inner HTML of the id="createddate" paragraph
+
+*/
+document.getElementById("createddate").innerHTML = "Created Date <br />Dec 1, 2018";
